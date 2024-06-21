9fa738d7f7f2ec896b9fb666034c1ea27ba3ced3
Alam <1871216+aflmp@users.noreply.github.com>
2024-06-20 18:22:08 -0700

    add gh action

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example
+
+go 1.22.4
+
+require github.com/someorgx/thursday v0.0.0-20240621010819-e20379cf1ad9
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/someorgx/thursday/hello"
+)
+
+func main() {
+	fmt.Println(hello.Hello("world"))
+}
